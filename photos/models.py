9560feef21d04f9aab4adf35db5575b81e64dc4d
JoyMbugua <wacumbugua@gmail.com>
2021-05-14 11:49:16 +0300

    added location and category models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
