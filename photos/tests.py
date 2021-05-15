623e4bb2365e193b82681b48a99b74c2ddac5d09
JoyMbugua <wacumbugua@gmail.com>
2021-05-15 15:41:28 +0300

    added instantiation tests for the 3 models

--- photos/tests.py
+++ photos/tests.py
@@ -1,13 +1,28 @@
 from django.test import TestCase
-from .models import Location, Category, Photos
+from .models import Location, Category, Photos, User
 
 class LocationModelTest(TestCase):
     def setUp(self):
-        pass
+        self.new_location = Location(name="Seoul")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_location, Location))
+
 class CategoryModelTest(TestCase):
     def setUp(self):
-        pass
+        self.new_category = Category(name="art")
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_category, Category))
+
 
 class PhotosModelTest(TestCase):
     def setUp(self):
-        pass
+        location = Location(name="Kenya")
+        owner = User(username='picasso')
+
+        self.new_pic = Photos(caption="christmas fun", location=location, owner=owner)
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_pic, Photos))
+
